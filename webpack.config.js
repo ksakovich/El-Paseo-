113955fc2d2a547f8d4a6d152a2adf18c0c759f1
Kirill Sakovich <kirill.s.sakovich@gmail.com>
2021-04-02 14:19:04 -0700

    another try to set up everything

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/index.tsx',
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'public')
     },
     devtool: 'inline-source-map',
     module: {
