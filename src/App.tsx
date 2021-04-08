870ca4aaa1cad760f336080badd5a6c68c9e86ed
Kirill Sakovich <kirill.s.sakovich@gmail.com>
2021-04-07 23:21:46 -0700

    cleaning up

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,11 @@ import Gallery from './containers/Gallery/Gallery'
 
 const App: React.FC = () =>
 {
-  function click()
-  {
-    console.log("!!!!!!!CLICKED")
-  }
   return (
     <div className="App">
 
       <p>Hello world from REACT! Before Gallery</p>
       <Gallery />
-      <button onClick={click}>Click</button>
-      <p>After Gallery</p>
     </div>
   );
 }
